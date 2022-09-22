d32ee94a2eb49548aaffbcc60c4a4cac1f8b5203
KurthaTheBurtha <kschimmel@waprep.org>
2022-09-21 19:20:31 -0700

    with scanner
    with scanner

--- src/homework.java
+++ src/homework.java
@@ -7,31 +7,17 @@ public class homework {
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
         HashMap<String,String> assignments = new HashMap<String,String>();
-        System.out.println("Add assignment - 1: Remove Assignment - 2: Modify Assignment - 3: View Assignments - 4");
+        System.out.println("Add assignment - 1: Remove Assignment - 2: View Assignments - 3");
         String response = in.nextLine();
         String ans = "";
-        //add assignment
         while(true) {
             if (response.equals("1")) {
-                System.out.println("Due Date");
-                String duedate = in.nextLine();
-                System.out.println("Assignment Name");
-                String assname = in.nextLine();
-                assignments.put(duedate,assname);
-                System.out.println("Would you like to add another one?");
-                ans = in.nextLine();
-                if(ans.equalsIgnoreCase("n")){
-                    ans="";
-                    break;
-                }
+                add(assignments);
             }
             else if(response.equals("2")){
-
+                remove(assignments);
             }
             else if(response.equals("3")){
-
-            }
-            else if(response.equals("4")){
                 print(assignments);
             }
                 System.out.println("Would you like to do anything else?");
@@ -39,6 +25,9 @@ public class homework {
                 if (ans.equalsIgnoreCase("n")) {
                     ans = "";
                     break;
+                } else {
+                    System.out.println("Add assignment - 1: Remove Assignment - 2: Modify Assignment - 3: View Assignments - 4");
+                    response = in.nextLine();
                 }
 
         }
@@ -54,8 +43,37 @@ public class homework {
             num++;
         }
     }
+    //adds an element to the HashMap
     public static HashMap<String,String> add(HashMap<String,String> assignments){
-        return null;
+        Scanner in = new Scanner(System.in);
+        String ans = "";
+        System.out.println("Due Date");
+        String duedate = in.nextLine();
+        System.out.println("Assignment Name");
+        String assname = in.nextLine();
+        assignments.put(duedate,assname);
+        System.out.println("Would you like to add another one?");
+        ans = in.nextLine();
+        if(ans.equalsIgnoreCase("y")){
+            add(assignments);
+        }
+        return assignments;
+    }
+    //removes an element from a HashMap
+    public static HashMap<String,String> remove(HashMap<String,String> assignments){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Which one would you like to remove? Enter a number");
+        print(assignments);
+        int ans = Integer.parseInt(in.nextLine());
+        int num=1;
+        for (HashMap.Entry<String,String> s : assignments.entrySet()){
+            if(num==ans){
+                assignments.remove(s.getKey(),s.getValue());
+                break;
+            }
+            num++;
+        }
+        return assignments;
     }
 
 }
