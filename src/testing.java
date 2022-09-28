bff88343dc589f99c520220296a3a69a74e45290
KurthaTheBurtha <kschimmel@waprep.org>
2022-09-27 22:23:51 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml
    #	work

--- src/testing.java
+++ src/testing.java
@@ -1,13 +1,19 @@
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Scanner;
 import java.util.Date;
 
 public class testing {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String word = input.nextLine();
-        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM");
-//        Date date = formatter.parse(word);
-        System.out.println("date");
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd");
+        String strDate = dateFormat.format(date);
+        System.out.println("Converted String: " + strDate);
+//        int dashindex = strDate.indexOf("-");
+//        String strmonth = strDate.substring(dashindex+1,dashindex+3);
+//        String strday = strDate.substring(dashindex+4,dashindex+7);
+//        System.out.println(strmonth);
+//        System.out.println(strday);
     }
 }
