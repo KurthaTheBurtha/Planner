d605ecfd3a28a4c167ec942277a73f4aa0127cd4
KurthaTheBurtha <kschimmel@waprep.org>
2022-09-28 22:44:35 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml
    #	work

--- src/testing.java
+++ src/testing.java
@@ -1,5 +1,6 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Scanner;
 import java.util.Date;
@@ -20,6 +21,11 @@ public class testing {
         System.out.println(isLater("09/28/2022","09/29/2022"));
         System.out.println(isLater("09/28/2022","10/29/2022"));
         System.out.println(isLater("1/23/2022","12/27/2021"));
+        ArrayList<Assignment> assignments = new ArrayList<>();
+        Assignment a = new Assignment("penis","poo","09/28/2022","Tuesday");
+        Assignment b = new Assignment("butt","poo","09/31/2022","Wednesday");
+        assignments.add(b);
+        assignments.add(a);
     }
     //dates come in the form MM/dd
     public static boolean isLater(String date1, String date2){
@@ -40,5 +46,19 @@ public class testing {
         }
         return false;
     }
+    //sorts the arraylist by due date
+    public static void sort(ArrayList<Assignment> assignments){
+        Assignment temp;
+        for(int i = 0; i< assignments.size()-1;i++){
+            for(int j = 0; j<assignments.size()-1-i;j++){
+                if(isLater(assignments.get(j).duedate,assignments.get(j+1).duedate)){
+//                    swap
+                }
+            }
+        }
+    }
+//    public static void swap(ArrayList<Assignment> a, int index1, int index2){
+//        ArrayList<Assignment>
+//    }
 
 }
